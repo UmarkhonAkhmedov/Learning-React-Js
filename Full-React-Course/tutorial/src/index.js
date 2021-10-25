9e4d39c7f909d5b5319a2cceb72a82ee67e29680
UmarkhonAkhmedov <umarkhonakhmedov2000@gmail.com>
2021-10-25 16:03:29 +0500

    Learning JSX Css JavaScript and Props

--- Full-React-Course/tutorial/src/index.js
+++ Full-React-Course/tutorial/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css'
 
 // Stateless functional component
 // Always return JSX
@@ -44,29 +45,62 @@ import ReactDom from 'react-dom';
 ///////////////////////////////////////////////////////////
 // Mini Book Project
 
+// function BookList(){
+//   return ( 
+//   <section className="booklist">
+//     <Book/>
+//   </section>
+//   );
+// }
+
+// const Book = () => {
+//   return (
+//   <article className="book">
+//     <Image/>
+//     <Title/>
+//     <Author/>
+//   </article>
+//   );
+// }
+
+// const Image = () => (
+//   <img src="https://images-na.ssl-images-amazon.com/images/I/81z6xJd6mrS._AC_UL200_SR200,200_.jpg" alt="Bestselling Book" />
+// )
+// const Title = () => <h1>Dune (Dune Chronicles, Book 1)</h1>
+// // const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Frank Herbert</h4>
+// const Author = () => <h4>Frank Herbert</h4>
+
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/71BMsKPqsIS._AC_UL200_SR200,200_.jpg",
+  title:'I love you to the Moon and Back',
+  author: 'Amelia Hepworth'
+}
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81z6xJd6mrS._AC_UL200_SR200,200_.jpg",
+  title:'Big Shot Diary of a Wimpy Kid Book 16',
+  author: 'Jeff Kinney'
+}
+
 function BookList(){
-  return ( 
-  <section>
-    <Book/>
-  </section>
-  );
-}
-
-const Book = () => {
   return (
-  <article>
-    <Image/>
-    <Title/>
-    <Author/>
-  </article>
+    <section className='booklist'>
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+    </section>
   );
 }
 
-const Image = () => (
-  <img src="https://images-na.ssl-images-amazon.com/images/I/81z6xJd6mrS._AC_UL200_SR200,200_.jpg" alt="Bestselling Book" />
-)
-const Title = () => <h1>Dune (Dune Chronicles, Book 1)</h1>
-const Author = () => <h4>Frank Herbert</h4>
+const Book = (props) => {
+  return (
+    <article className='book'>   
+      <img src={props.img} alt='Book'/>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  )
+}
+
 
 
 ReactDom.render(<BookList/>, document.getElementById('root'));
