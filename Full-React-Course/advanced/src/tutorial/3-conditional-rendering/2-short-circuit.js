8100c7c5f7aa23510235bdfb63a9313ab0fce441
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2021-11-02 05:11:25 +0500

    Learning show-hide conditional rendering and basic forms in JS

--- Full-React-Course/advanced/src/tutorial/3-conditional-rendering/2-short-circuit.js
+++ Full-React-Course/advanced/src/tutorial/3-conditional-rendering/2-short-circuit.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 // short-circuit evaluation
 // ternary operator
 
-const ShortCircuit = () => {const [text, setText] = useState('');
+const ShortCircuit = () => {
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
   const firstValue = text || 'hello world';
   const secondValue = text && 'hello world';
 
   return (
     <>
-      <h1>{firstValue}</h1>
-      <h1>value: {secondValue}</h1>
+      <h1>{text || 'john doe'}</h1>
+      <button className='btn'>toggle error</button>
+      {text && <h1>hello world</h1>}
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>there is no error...</p> : <p>there is error</p>}
     </>
   );
 };
