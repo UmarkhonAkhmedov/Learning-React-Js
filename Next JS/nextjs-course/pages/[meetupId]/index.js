91968b71cf85360d4fa3be8b0240f445cc04f079
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-01-17 01:26:14 +0500

    Create Meetupdetail component

---
+++ Next JS/nextjs-course/pages/[meetupId]/index.js
@@ -0,0 +1,11 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetupDetails() {
+  return (
+    <>
+      <MeetupDetail image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/tcx040121pinaultmuseum-004-1621303048.jpg" title="First Meetup" address="Some Streent, Some city" decription="Some text"/>
+    </>
+  )
+}
+
+export default MeetupDetails
